fc7632d7407fcdd876f2d9423215fe0855b140be
warren <warrendodsworth@outlook.com>
2019-11-13 05:15:23 +1100

    clean app.component - fcm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -12,36 +8,7 @@ import { ToastrService } from 'ngx-toastr';
 export class AppComponent implements OnInit {
   title = 'Inspire Me - Men of Melbourne';
 
-  navCollapsed = true;
-  notification: any;
+  constructor() {}
 
-  constructor(
-    public route: ActivatedRoute,
-    public router: Router,
-    public afs: AngularFirestore,
-    public http: HttpClient,
-    public toastr: ToastrService
-  ) {}
-
-  async ngOnInit() {
-    // this.auth.user$.subscribe(user => {
-    //   this.user = user;
-    //   if (user) {
-    //     this.fcm.setupFCMToken(user);
-    //     this.fcm.listenToNotifications();
-    //   }
-    // });
-    // this.fcm.currentMessage.subscribe(msg => {
-    //   if (msg) {
-    //     this.notification = (<any>msg).notification;
-    //     this.toastr.info(this.notification.body, this.notification.title);
-    //   }
-    // });
-  }
-
-  isView(name) {
-    let path = this.route.pathFromRoot.join('/');
-    path = path.substring(1, path.length).trim();
-    return path.indexOf(name) > -1;
-  }
+  async ngOnInit() {}
 }
